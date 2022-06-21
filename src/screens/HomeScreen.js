4b4aaafd236448c95549f9399ca6ad806c9ba1ac
shindonghwi <ehdgnl8940@naver.com>
2022-06-22 01:00:47 +0900

    feat: add expo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi There Shin Dong Hwi !</Text>;
+  return <Text style={styles.text}>Shin Dong Hwi !!!!!</Text>;
 };
 
 const styles = StyleSheet.create({
