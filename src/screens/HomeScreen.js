f6781e0252adb65c510f2040dc47070f05e26602
shindonghwi <ehdgnl8940@naver.com>
2022-06-22 15:30:55 +0900

    feat: 화면 전환기능 추가

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Shin Dong Hwi !!!!!</Text>;
+const HomeScreen = ({ navigation }) => {
+  console.log(navigation);
+
+  return (
+    <View>
+      <Text>Hi There!</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+
+      <Button
+        title="Go to List Deni"
+        onPress={() => navigation.navigate("List")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,28 +3,35 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
+    { name: "Friend #1", age: 12 },
+    { name: "Friend #2", age: 23 },
+    { name: "Friend #3", age: 42 },
+    { name: "Friend #4", age: 52 },
+    { name: "Friend #5", age: 32 },
+    { name: "Friend #6", age: 72 },
   ];
 
   return (
     <FlatList
+      horizontal={false} // 스크롤 방향
+      showsVerticalScrollIndicator={false} // 스크롤 인디케이터 표시
       keyExtractor={(friends) => friends.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age: {item.age}
+          </Text>
+        );
       }}
     />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 24,
+  },
+});
 
 export default ListScreen;
